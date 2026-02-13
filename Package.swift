4dfcd1a29c9f7f323bc5cd4c939984e3424cd60e
Carl Sheppard <carl@antarianlogic.com>
2026-02-13 10:31:03 -0800

    initial commit

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "tidal-api-interchange-kit",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v12)
+    ],
+    products: [
+        .library(name: "TidalAPIInterchangeKit",
+                 targets: ["TidalAPIInterchangeKit"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/antarianLogic/interchange", from: "1.0.4")
+    ],
+    targets: [
+        .target(name: "TidalAPIInterchangeKit",
+                dependencies: [.product(name: "Interchange", package: "interchange")]),
+        .testTarget(name: "TidalAPIInterchangeKitTests",
+                    dependencies: ["TidalAPIInterchangeKit"])
+    ]
+)
---
+++ README.md
@@ -0,0 +1,3 @@
+# tidal-api-interchange-kit
+
+## A Swift package containing API methods and response models for interacting with the TIDAL [API](https://tidal-music.github.io/tidal-api-reference) via [Interchange](https://github.com/antarianLogic/interchange)
